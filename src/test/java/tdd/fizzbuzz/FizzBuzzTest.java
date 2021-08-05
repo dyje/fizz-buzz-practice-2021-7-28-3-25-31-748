948b9dcb61abb248dbdd459f689c5326ab63c70e
dyje <dyje@oocl.com>
2021-08-05 13:46:19 +0800

    [FizzBuzz][Jesse] test: add test case should_return_order_number_when_count_of_given_normal_case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_return_order_number_when_count_of_given_normal_case() {
+        // given
+        int givenNumber = 2;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String result = fizzBuzz.countOf(givenNumber);
+
+        // then
+        assertThat(result).isEqualTo("2");
+    }
 }
