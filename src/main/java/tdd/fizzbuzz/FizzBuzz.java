298f8c5cef6a4c9a93b05a5bf471b0c949910295
dyje <dyje@oocl.com>
2021-08-05 14:00:41 +0800

    [FizzBuzz][Jesse] feat: implementation for should_return_buzz_when_count_of_multiple_of_five

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -6,7 +6,10 @@ public class FizzBuzz {
 
         if (givenNumber%3 == 0){
             return "Fizz";
-        }else {
+        }else if(givenNumber%5 == 0){
+            return "Buzz";
+        }
+        else {
             return String.valueOf(givenNumber);
         }
     }
