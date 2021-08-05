14847799a6c52c44ccb8a71c02a780c0e10ed7ed
dyje <dyje@oocl.com>
2021-08-05 13:56:22 +0800

    [FizzBuzz][Jesse] test: add test case should_return_fizz_when_count_of_multiple_of_three

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -18,4 +18,17 @@ public class FizzBuzzTest {
         // then
         assertThat(result).isEqualTo("2");
     }
+
+    @Test
+    void should_return_fizz_when_count_of_multiple_of_three() {
+        //given
+        int givenNumber = 9;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+        String result = fizzBuzz.countOf(givenNumber);
+
+        //then
+        assertThat(result).isEqualTo("Fizz");
+    }
 }
